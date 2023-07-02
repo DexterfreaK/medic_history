f9038767320175d8c737feb546aec3d67f373ac3
Dexter-freak <F20212792@pilani.bits-pilani.ac.in>
2023-07-02 19:48:44 +0530

    vercel rollback

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "www.myopd.in", "png.pngtree.com"],
-    routes: [
-      {
-        src: "/(.*)",
-        headers: {
-          "x-forwarded-proto": "https",
-        },
-        methods: ["GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"],
-      },
-    ],
   },
 };
 
